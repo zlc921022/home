17cafbae2a20ae3e8d0034295fa5e09626d8d014
zlc <zlc921022@163.com>
2020-05-06 16:36:31 +0800

    测试数据

--- app/src/main/java/com/xiaochen/home/MainActivity.kt
+++ app/src/main/java/com/xiaochen/home/MainActivity.kt
@@ -2,11 +2,15 @@ package com.xiaochen.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv)
+        tv.text = "测试数据"
     }
 }
